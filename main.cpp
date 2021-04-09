4e137704e3c4c289a50c07f6ea0725752709d131
Xoxol <48468468+karsar99@users.noreply.github.com>
2021-04-09 21:57:57 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,42 @@
-//Напишите программу, в которой создается квадратная матрица(реализуется через
-//двумерный массив). Матрица заполняется случайными числами, после чего выполняется
-//«поворот по часовой стрелке»: первый столбец становится первой строкой, второй столбец
-//становится второй строкой, и так далее.
 #include <iostream>
 
 using namespace std;
 
 asm(".intel_syntax noprefix\n\t");
 
-int main(){
+int main()
+{
 	const int size = 5;
 	int** src = new int*[size];
 	int** dst = new int*[size];
 
 	for (size_t i = 0; i < size; ++i){
-		src[i] = new int[size];
-		dst[i] = new int[size];
-		for (size_t j = 0; j < size; ++j){
-			src[i][j] = rand() % 9;
-		}
+	src[i] = new int[size];
+	dst[i] = new int[size];
+	for (size_t j = 0; j < size; ++j){
+	src[i][j] = rand() % 9;
 	}
+}
 
 	for (size_t i = 0; i < size; ++i){
-		for (size_t j = 0; j < size; ++j) 
-			cout << src[i][j] << ' ';
-		cout << endl;
+	for (size_t j = 0; j < size; ++j) 
+	cout << src[i][j] << ' ';
+	cout << endl;
 	}
 	cout << endl;
 
-	//for (size_t i = 0; i < size; ++i){
-	//	for (size_t j = 0; j < size; ++j){
-	//		dst[j][size - i - 1] = src[i][j];
-	//	}
-	//}
+//for (size_t i = 0; i < size; ++i){
+//	for (size_t j = 0; j < size; ++j){
+//		dst[j][size - i - 1] = src[i][j];
+//	}
+//}
 
 	asm(
-		"	mov	rax, 0\n\t"  				// i = 0
+		"	mov	rax, 0\n\t"	// i = 0
 		".i_loop:\n\t"
 		"	cmp	rax, rcx\n\t"
 		"	jge	.EXIT\n\t"
-		"	mov	rbx, 0\n\t"					// j = 0
+		"	mov	rbx, 0\n\t"	// j = 0
 		".j_loop:\n\t"
 		"	cmp	rbx, rcx\n\t"
 		"	jge	inc_i_loop\n\t"
@@ -50,10 +47,10 @@ int main(){
 		"	sub r10, 1\n\t"					// size - i - 1
 		"	mov r13, [rdi + rbx * 8]\n\t" 	// dst[j]
 		"	mov [r13 + r10 * 4], r8d\n\t"  	// dst[j][size - i - 1]
-		"	add	rbx, 1\n\t"					// ++j
+		"	add	rbx, 1\n\t"	// ++j
 		"	jmp	.j_loop\n\t"
 		"inc_i_loop:\n\t"
-		"	add	rax, 1\n\t" 				// ++i
+		"	add	rax, 1\n\t"	// ++i
 		"	jmp	.i_loop\n\t"
 		".EXIT:\n\t"
 		:
@@ -61,11 +58,12 @@ int main(){
 		:
 	);
 
-	for (size_t i = 0; i < size; ++i){
+	for (size_t i = 0; i < size; ++i)
+	{
 		for (size_t j = 0; j < size; ++j)
-			cout << dst[i][j] << ' ';
+		cout << dst[i][j] << ' ';
 		cout << endl;
 	}
 
-	return 0;
+return 0;
 }
